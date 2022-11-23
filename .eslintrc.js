2102b960cce3b7da184e106c0fba77cb60525e00
hosono kotaro <hosono1985@gmail.com>
2022-11-23 22:03:29 +0900

    feat: Add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const rules = require('@hosonokotaro/eslint-rules')
+const eslintRules = require('@hosonokotaro/eslint-rules')
 
 module.exports = {
-  ...rules,
+  ...eslintRules,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
 }
