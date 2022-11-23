13ef0ad2f8aa612271a3576ac7f4288c29a5242f
hosono kotaro <hosono1985@gmail.com>
2022-11-23 22:03:29 +0900

    feat: Add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  typescript: {
+    check: true,
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src/components/'),
+    }
+
+    return config
+  },
+}
