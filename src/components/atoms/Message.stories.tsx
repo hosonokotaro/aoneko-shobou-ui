ee795e1974f357a9f21ecf9ec4105a4b67316811
hosono kotaro <hosono1985@gmail.com>
2022-11-25 15:24:56 +0900

    fix: Update sample stories

--- src/components/atoms/Message.stories.tsx
+++ src/components/atoms/Message.stories.tsx
@@ -3,24 +3,17 @@ import React from 'react'
 
 import Message from '@/atoms/Message'
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'atoms/Message',
   component: Message,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    no: {
-      control: 'number',
-      defaultValue: 0,
-    },
+  args: {
+    no: 0,
   },
 } as ComponentMeta<typeof Message>
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Message> = (args) => <Message {...args} />
 
 export const Primary = Template.bind({})
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   text: 'Primary',
 }
