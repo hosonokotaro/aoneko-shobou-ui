63dbc1cb6403d2ff4a0c869d2407e8276fc5c831
hosono kotaro <hosono1985@gmail.com>
2022-12-12 01:05:52 +0900

    feat: Add common list component

---
+++ src/components/molecules/CommonList.stories.tsx
@@ -0,0 +1,48 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import CommonList, { CommonItem } from '@/molecules/CommonList'
+
+export default {
+  title: 'molecules/CommonList',
+  component: CommonList,
+} as ComponentMeta<typeof CommonList>
+
+const Template: ComponentStory<typeof CommonList> = (args) => (
+  <CommonList {...args} />
+)
+
+const commonList: CommonItem[] = [
+  {
+    title: 'ホットコーヒー',
+    description: '￥400（税込）',
+  },
+  {
+    title: 'ラテマキアート',
+    description: '￥500（税込）',
+  },
+  {
+    title: 'ノンカフェインコーヒー',
+    description: '￥450（税込）',
+  },
+  {
+    title: 'アイスコーヒー',
+    description: '￥400（税込）',
+  },
+  {
+    title: 'アイスカフェオレ',
+    description: '￥450（税込）',
+  },
+  {
+    title: 'オレンジジュース',
+    description: '￥400（税込）',
+  },
+  {
+    title: 'アップルジュース',
+    description: '￥400（税込）',
+  },
+]
+
+export const Default = Template.bind({})
+Default.args = {
+  commonList,
+}
