131635a170b22e5c05ccd990d484307e97d53c29
hosono kotaro <hosono1985@gmail.com>
2022-12-17 01:54:16 +0900

    feat: Change build system

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from '@/atoms/Anchor'
+export { default as Anchor } from '@/atoms/Anchor'
+export * from '@/atoms/AnnotationText'
+export { default as AnnotationText } from '@/atoms/AnnotationText'
