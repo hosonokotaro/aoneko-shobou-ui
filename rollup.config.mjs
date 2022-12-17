a4c585ab100a8f6aeefcdf36d8aa241cd9c1764f
hosono kotaro <hosono1985@gmail.com>
2022-12-17 16:56:35 +0900

    feat: Change typescript plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import typescript from 'rollup-plugin-typescript2'
 
 import packageJson from './package.json' assert { type: 'json' }
 
