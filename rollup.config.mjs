eb3a655d6443f12a1988ca41a63acdc48fbb9bca
hosono kotaro <hosono1985@gmail.com>
2022-12-18 14:23:50 +0900

    fix: Remove sourcemap, Update build command

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -13,12 +13,10 @@ export default {
     {
       file: packageJson.main,
       format: 'cjs',
-      sourcemap: true,
     },
     {
       file: packageJson.module,
       format: 'esm',
-      sourcemap: true,
     },
   ],
   plugins: [
