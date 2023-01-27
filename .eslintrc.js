103d4c07d445cdb85a922f2fed44379a79d114ab
hosono kotaro <hosono1985@gmail.com>
2023-01-28 01:16:22 +0900

    feat: Add remove unused import plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ const eslintRules = require('@hosonokotaro/eslint-rules')
 
 module.exports = {
   ...eslintRules,
+  plugins: [...eslintRules.plugins, 'unused-imports'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
@@ -16,5 +17,15 @@ module.exports = {
     ...eslintRules.rules,
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 }
