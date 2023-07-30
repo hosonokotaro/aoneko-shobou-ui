da2d3a7b51617863b80223d0f402c218c0b1d70d
hosono kotaro <hosono1985@gmail.com>
2023-07-31 08:38:28 +0900

    chore: Update version

--- dist/components/atoms/InputText.d.ts
+++ dist/components/atoms/InputText.d.ts
@@ -2,6 +2,6 @@ import { ComponentProps } from 'react';
 export declare type InputTextProps = {
     className?: string;
     elementProps?: ComponentProps<'input'>;
-    placeholder: string;
+    placeholder?: string;
 };
 export declare const InputText: ({ className, elementProps, placeholder, }: InputTextProps) => JSX.Element;
--- dist/components/atoms/TextArea.d.ts
+++ dist/components/atoms/TextArea.d.ts
@@ -2,6 +2,6 @@ import { ComponentProps } from 'react';
 export declare type TextAreaProps = {
     className?: string;
     elementProps?: ComponentProps<'textarea'>;
-    placeholder: string;
+    placeholder?: string;
 };
 export declare const TextArea: ({ className, elementProps, placeholder, }: TextAreaProps) => JSX.Element;
