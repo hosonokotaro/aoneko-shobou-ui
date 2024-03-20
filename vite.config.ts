0336e7377fcc67f4827df3e9176e87eefcf4cfc3
hosono kotaro <hosono1985@gmail.com>
2024-03-20 15:44:07 +0900

    fix: Update build system

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-// import resolve from '@rollup/plugin-node-resolve'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
       name: 'index',
       fileName: 'index',
     },
+    sourcemap: true,
     rollupOptions: {
       external: [
         '@googlemaps/js-api-loader',
@@ -36,21 +35,8 @@ export default defineConfig({
           swiper: 'Swiper',
         },
       },
-      plugins: [
-        // peerDepsExternal(),
-        // resolve(),
-        // commonjs(),
-        // terser(),
-        // typescript({
-        //   typescript: ttypescript,
-        //   exclude: ['**/*.stories.tsx'],
-        // }),
-      ],
+      plugins: [],
     },
   },
-  plugins: [
-    tsconfigPaths(),
-    vanillaExtractPlugin(),
-    dts({ rollupTypes: true }),
-  ],
+  plugins: [tsconfigPaths(), react(), vanillaExtractPlugin()],
 })
