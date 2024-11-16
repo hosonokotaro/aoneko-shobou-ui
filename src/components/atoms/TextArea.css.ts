9626b109c5ec06894facd1b7ea4632ce5c101800
hosono kotaro <hosono1985@gmail.com>
2024-11-16 17:43:46 +0900

    fix: prevent tap zoom

--- src/components/atoms/TextArea.css.ts
+++ src/components/atoms/TextArea.css.ts
@@ -1,12 +1,14 @@
 import { style } from '@vanilla-extract/css'
 
 import { BORDER_COLOR, FONT_COLOR } from '@/const/color'
-import { BLOCK_WIDTH, BORDER_RADIUS, SPACE } from '@/const/size'
+import { BLOCK_WIDTH, BORDER_RADIUS, INLINE_SIZE, SPACE } from '@/const/size'
 
 export const textarea = style({
   width: BLOCK_WIDTH.FULL,
   height: '16em',
   padding: `${SPACE.M} ${SPACE.S}`,
+  lineHeight: INLINE_SIZE.XXL,
+  fontSize: INLINE_SIZE.L,
   border: `1px solid ${BORDER_COLOR.GRAY}`,
   color: FONT_COLOR.BLACK,
   borderRadius: BORDER_RADIUS.S,
