f086880e9c989b4343c7092cf2e85ab6598ec1bd
hosono kotaro <hosono1985@gmail.com>
2024-11-16 17:29:18 +0900

    fix: prevent tap zoom (#98)
    * fix: prevent tap zoom
    
    * chore: Create new tag v0.5.7

--- src/components/atoms/InputText.css.ts
+++ src/components/atoms/InputText.css.ts
@@ -1,11 +1,12 @@
 import { style } from '@vanilla-extract/css'
 
 import { BORDER_COLOR, FONT_COLOR } from '@/const/color'
-import { BLOCK_WIDTH, BORDER_RADIUS, SPACE } from '@/const/size'
+import { BLOCK_WIDTH, BORDER_RADIUS, INLINE_SIZE, SPACE } from '@/const/size'
 
 export const inputText = style({
   width: BLOCK_WIDTH.FULL,
   padding: `${SPACE.M} ${SPACE.S}`,
+  fontSize: INLINE_SIZE.L,
   border: `1px solid ${BORDER_COLOR.GRAY}`,
   color: FONT_COLOR.BLACK,
   borderRadius: BORDER_RADIUS.S,
