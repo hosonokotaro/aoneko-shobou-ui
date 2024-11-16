3fafc103f535f153f196f086f8d274722499bf59
hosono kotaro <hosono1985@gmail.com>
2024-11-16 17:23:51 +0900

    fix: prevent tap zoom

--- src/components/atoms/Select.css.ts
+++ src/components/atoms/Select.css.ts
@@ -1,12 +1,12 @@
 import { style } from '@vanilla-extract/css'
 
 import { BORDER_COLOR, FONT_COLOR } from '@/const/color'
-import { BLOCK_WIDTH, BORDER_RADIUS, BUTTON_SIZE, SPACE } from '@/const/size'
+import { BLOCK_WIDTH, BORDER_RADIUS, INLINE_SIZE, SPACE } from '@/const/size'
 
 export const select = style({
   width: BLOCK_WIDTH.FULL,
-  height: BUTTON_SIZE.M.HEIGHT,
   padding: `${SPACE.M} ${SPACE.XS}`,
+  fontSize: INLINE_SIZE.L,
   border: `1px solid ${BORDER_COLOR.GRAY}`,
   color: FONT_COLOR.BLACK,
   borderRadius: BORDER_RADIUS.S,
