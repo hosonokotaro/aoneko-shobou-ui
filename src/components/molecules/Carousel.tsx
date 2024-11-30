b654962f452dc27dd20f030ca0b643038215f47a
hosono kotaro <hosono1985@gmail.com>
2024-11-30 18:53:41 +0900

    fix: Update carousel component

--- src/components/molecules/Carousel.tsx
+++ src/components/molecules/Carousel.tsx
@@ -2,6 +2,7 @@ import 'swiper/css'
 import 'swiper/css/pagination'
 
 import { map } from 'lodash-es'
+import { ReactElement } from 'react'
 import { Autoplay, Pagination } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
@@ -11,46 +12,52 @@ import * as Styles from '@/molecules/Carousel.css'
 export type CarouselProps = {
   /** Atoms の Image component の props の配列を受け取る */
   imageList: ImageProps[]
+  bottomContent?: ReactElement
 }
 
 const renderBullet = (_: number, className: string) => {
   return `<button class="${className}"><span class="inner"></span></button>`
 }
 
-export const Carousel = ({ imageList }: CarouselProps) => {
+export const Carousel = ({ imageList, bottomContent }: CarouselProps) => {
   return (
-    <Swiper
-      className={Styles.carousel}
-      modules={[Autoplay, Pagination]}
-      loop={true}
-      slidesPerView={1}
-      speed={1200}
-      autoplay={{
-        delay: 4000,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: true,
-      }}
-      pagination={{
-        clickable: true,
-        renderBullet,
-        bulletClass: 'custom-bullet-style',
-        bulletActiveClass: 'custom-bullet-style-active',
-      }}
-      centeredSlides={true}
-      spaceBetween={40}
-    >
-      {map(imageList, ({ src, alt, width, height, loading }, index) => (
-        <SwiperSlide key={index} className={Styles.slide}>
-          <Image
-            dataStyleProps={{ 'data-parent-component': 'Carousel' }}
-            src={src}
-            alt={alt}
-            width={width}
-            height={height}
-            loading={loading}
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className={Styles.carouselWrapper}>
+      <Swiper
+        className={Styles.carousel}
+        modules={[Autoplay, Pagination]}
+        loop={true}
+        slidesPerView={1}
+        speed={1200}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        pagination={{
+          clickable: true,
+          renderBullet,
+          bulletClass: 'custom-bullet-style',
+          bulletActiveClass: 'custom-bullet-style-active',
+        }}
+        centeredSlides={true}
+        spaceBetween={40}
+      >
+        {map(imageList, ({ src, alt, width, height, loading }, index) => (
+          <SwiperSlide key={index} className={Styles.slide}>
+            <Image
+              dataStyleProps={{ 'data-parent-component': 'Carousel' }}
+              src={src}
+              alt={alt}
+              width={width}
+              height={height}
+              loading={loading}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {bottomContent && (
+        <div className={Styles.bottomContent}>{bottomContent}</div>
+      )}
+    </div>
   )
 }
